26c7b4da3756c02e0a292289795f50c0e4d2ff5c
yungstunna241 <truemudbaby@gmail.com>
2025-07-19 21:20:22 +0400

    add manager login

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ process.on('uncaughtException', function(err) {
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const User = require('./models/User')
 
 const app = express()
 
 app.use(cors({
   origin: '*',
   credentials: true
-}));
+}))
 app.use(express.json())
 
 mongoose.connect('mongodb+srv://truemudbaby:1paroliparoli@crm-base.ninskus.mongodb.net/crm?retryWrites=true&w=majority&appName=crm-base')
@@ -42,40 +44,4 @@ app.get('/contacts', async (req, res) => {
     res.json(contacts)
   } catch (err) {
     console.error('GET /contacts error:', err)
-    res.status(500).json({ error: 'Failed to fetch contacts' })
-  }
-})
-
-app.post('/contacts/:id/update', async (req, res) => {
-  try {
-    const { kyc, result } = req.body
-    await Contact.findByIdAndUpdate(req.params.id, { kyc, result })
-    res.sendStatus(200)
-  } catch (err) {
-    console.error('POST /contacts/:id/update error:', err)
-    res.status(500).json({ error: 'Failed to update contact' })
-  }
-})
-
-const port = process.env.PORT || 4000
-
-const jwt = require('jsonwebtoken');
-const User = require('./models/User');
-
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: 'User not found' });
-
-    const isMatch = password === user.password;
-    if (!isMatch) return res.status(401).json({ message: 'Wrong password' });
-
-    const token = jwt.sign({ id: user._id, role: user.role }, 'secretkey');
-    res.json({ token, user });
-  } catch (err) {
-    res.status(500).json({ message: 'Server error' });
-  }
-});
-app.listen(port, () => console.log(`Backend running on http://localhost:${port}`))
+    res.stat
